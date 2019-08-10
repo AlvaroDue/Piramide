86352b0e1c49bbd6bf5e8bfc73d37bcdf7aa05b0
AlvaroDue <alvarodue@gmail.com>
2019-08-10 11:41:22 -0500

    Se cambia la forma de selecionar color

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 
 var obtenBase = document.getElementById("base");
 var obtenSymbol = document.getElementById("symbol");
-var obtenColor = document.getElementById("color");
 var obtenPiramide = document.getElementById("piramide");
+var radios = document.getElementsByName('radio');
 
 function creaPiramide() {
     base = obtenBase.value;
@@ -10,11 +10,43 @@ function creaPiramide() {
     linea = '';
     
     piramide.innerHTML = linea;
-    color = document.getElementById("color").value;
+
+    for (var i = 0, length = radios.length; i < length; i++)
+    {
+        if (radios[i].checked) {
+            checado = radios[i].value;
+
+           /* if ( checado == 1) {
+                color = "#c82a54"
+            };*/
+
+            switch (checado) {
+                case '1':
+                  console.log('color 1');
+                  break;
+                case '2':
+                  console.log('color 2');
+                  break;
+                case '3':
+                  console.log('color 3');
+                  break;
+                case '4':
+                  console.log('color 4');
+                  break;
+                
+                default:
+                  console.log('negro');
+              }
+
+            break;
+        }
+    }
 
     for (var i = 0; i < base; i++) {
         piramide.innerHTML += '<p class="line">' + (linea += ('<span class="symbol">' + symbol + '</span>')) + '</p>';
     }
-    piramide.style.color = color;
+    //piramide.style.color = color;
 }
+
+
 
